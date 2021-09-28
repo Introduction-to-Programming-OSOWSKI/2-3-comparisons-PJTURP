0762a395e2f5da5a2814691614684b11f1e224ab
PJTURP <821042@mystma.org>
2021-09-28 09:27:37 -0500

    attempt 1

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
-#WRITE YOUR CODE IN THIS FILE
+#greaterThan Function
+def greaterThan(x,y):
+    if x > y:
+        return True
+
+    else:
+        return False
+
+#lessThan Function
+def lessThan(x,y):
+    if x < y:
+        return True
+    
+    else:
+        return False
+
+#equalTo Function
+def equalTo(x,y):
+    if x == y:
+        return True
+    
+    else:
+        return False
+
+#greaterOrEqualTo Function
+def greaterOrEqualTo(x,y):
+    if x >= y:
+        return True
+
+    else:
+        return False
+
+#lessThanOrEqualTo Function
+def lessThanOrEqualTo(x,y):
+    if x <= y:
+        return True
+
+    else:
+        return False
